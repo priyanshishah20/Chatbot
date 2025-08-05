7f1343b1114e26d79b17f4d4d18b12ef6be995d1
Priyanshi Shah <shahpriyanshi1025@gmail.com>
2025-08-05 23:13:28 +0530

    chtbot

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import ChatInput from './chatbot/ChatInput'
 
 function App() {
 
+  const [chatMsg, setChatMsg] = useState([
+          { id:'id1', message: 'Hi, how are you?', sender: 'user' },
+          { id:'id2', message: 'Hi, I\'m chatbot. How you doing?', sender: 'robot' },
+          { id:'id3', message: 'Doing great, thanks.', sender: 'user' },
+      ]);
+
   return (
     <>
-      <ChatInput />
+      <ChatInput chatMsg={chatMsg} setChatMsg={setChatMsg} />
     </>
   )
 }
