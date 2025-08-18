e0e3b2943f693bcba32ffc20a522b5b0cbad1017
Priyanshi Shah <shahpriyanshi1025@gmail.com>
2025-08-18 21:49:03 +0530

    top and bottom input

--- src/chatbot/ChatInput.jsx
+++ src/chatbot/ChatInput.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const ChatInput = ({ saveInput, saveText, sendMsg }) => {
     return (
         <>
-            <div className='container flex py-4 fixed mx-auto bg-white w-[94%] lg:w-1/2 bottom-0'>
+            <div className='container flex py-4 fixed mx-auto bg-white w-[94%] lg:w-1/2 top-0'> {/* bottom-0 */}
                 <input type="text" onChange={saveInput} value={saveText} placeholder='Send a message to Chatbot' className='w-full border border-gray-500 outline-none p-2 rounded-lg' />
                 <button type='button' onClick={sendMsg} className='ml-3 bg-[#198754] text-white rounded-lg p-2 px-4 cursor-pointer'>Send</button>
             </div>
